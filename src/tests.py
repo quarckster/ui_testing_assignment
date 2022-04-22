9946f9768ec2096217dc9389b3d3154dc15d975e
Dmitry Misharov <dmisharo@redhat.com>
2022-04-22 09:34:12 +0200

    Added docstrings to tests

--- src/tests.py
+++ src/tests.py
@@ -15,15 +15,26 @@ def selenium():
 
 
 def test_login():
-    pass
-
-
-def test_logout():
+    """Test login with correct credentials
+    Steps:
+        1. Go to login page "http://microblog:5000/auth/login"
+        2. Fill in username and password
+        3. Click submit
+        4. Check if user is logged in
+        5. Logout
+        6. Check if user is logged out
+    """
     pass
 
 
 def test_register(selenium):
-    # Load the page for registration
+    """Test register with correct credentials
+    Steps:
+        1. Go to register page "http://microblog:5000/auth/register"
+        2. Fill in username, email, password and confirm password
+        3. Click submit
+        4. Check if alert has expected text
+    """
     selenium.get("http://microblog:5000/auth/register")
     # Fill in the fields
     username_field = selenium.find_element_by_id("username")
@@ -43,24 +54,79 @@ def test_register(selenium):
 
 
 def test_edit_profile():
+    """Test edit profile with correct credentials
+    Steps:
+        1. Go to edit profile page "http://microblog:5000/edit_profile"
+        2. Update "About me" fields
+        3. Click submit
+        4. Check if alert has expected text
+        5. Check if "About me" fields are updated
+        6. Revert all changes
+    """
     pass
 
 
 def test_post_create_delete():
+    """Test create and delete post
+    Steps:
+        1. Go to main page "http://microblog:5000/"
+        2. Fill "Say something" field
+        3. Click submit
+        4. Check if the post is created
+    """
     pass
 
 
 def test_search():
+    """Test search
+    Steps:
+         1. Go to main page "http://microblog:5000/"
+         2. Fill "Search" field
+         3. Send "Enter" key
+         4. Check if search results are empty
+    """
     pass
 
 
 def test_messages():
+    """Test messages
+    Steps:
+        1. Create a new user
+        2. Login as the new user
+        3. Go to main user page
+        4. Click on "Send private message"
+        5. Fill in "Message" field
+        6. Click submit
+        7. Check if alert has expected text
+        8. Logout
+        9. Login as the main user
+        10. Go to messages
+        11. Check if message from another user is available
+    """
     pass
 
 
 def test_follow():
+    """Test follow
+    Steps:
+        1. Create a new user
+        2. Login as the new user
+        3. Go to main user page
+        4. Click on "Follow"
+        5. Check if alert has expected text
+        6. Logout
+        7. Login as the main user
+        8. Go to main user page
+        9. Check if followers count is equal to 1
+    """
     pass
 
 
 def test_explore():
+    """Test explore
+    Steps:
+        1. Submit three posts
+        2. Go to explore page "http://microblog:5000/explore"
+        3. Check if posts are available
+    """
     pass
